76ac8065dfc7caac3221e9c78161be17236473ea
Jerry Sievert <code@legitimatesounding.com>
2016-01-01 13:40:17 -0800

    initial import of code

---
+++ README.md
@@ -0,0 +1,19 @@
+# Simple-CI
+
+A very simple continuous integration server.
+
+## .simple-ci.json
+
+```js
+{
+  "setup": [
+    "make clean"
+  ],
+  "build": [
+    "make"
+  ],
+  "teardown": [
+
+  ]
+}
+```
---
+++ migrations/20151230125403624-project-table.js
@@ -0,0 +1,9 @@
+var db = require('../lib/db');
+
+exports.up = function(next){
+  db.query('CREATE TABLE project (id SERIAL, name TEXT, repo TEXT, created TIMESTAMP, updated TIMESTAMP)', next);
+};
+
+exports.down = function(next){
+  db.query('DROP TABLE IF EXISTS project', next);
+};
