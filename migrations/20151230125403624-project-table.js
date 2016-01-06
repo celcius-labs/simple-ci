84338b1bf896200930d1280e34081159751eeecc
Jerry Sievert <code@legitimatesounding.com>
2016-01-06 15:56:11 -0800

    change to use time zone for dates

--- migrations/20151230125403624-project-table.js
+++ migrations/20151230125403624-project-table.js
@@ -1,7 +1,7 @@
 var db = require('../lib/db');
 
 exports.up = function(next){
-  db.query('CREATE TABLE project (id SERIAL PRIMARY KEY, name TEXT, repo TEXT, created TIMESTAMP, updated TIMESTAMP)', next);
+  db.query('CREATE TABLE project (id SERIAL PRIMARY KEY, name TEXT, repo TEXT, created TIMESTAMP WITH TIME ZONE, updated TIMESTAMP WITH TIME ZONE)', next);
 };
 
 exports.down = function(next){
--- migrations/20160101144456603-results-table.js
+++ migrations/20160101144456603-results-table.js
@@ -1,7 +1,7 @@
 var db = require('../lib/db');
 
 exports.up = function(next){
-  db.query('CREATE TABLE results (id SERIAL, project_id INTEGER references project(id), init_error TEXT, setup_error TEXT, setup_stdout TEXT, setup_stderr TEXT, build_error TEXT, build_stdout TEXT, build_stderr TEXT, test_error TEXT, test_stdout TEXT, test_stderr TEXT, started TIMESTAMP, ended TIMESTAMP, error BOOL, platform TEXT, created TIMESTAMP, updated TIMESTAMP)', next);
+  db.query('CREATE TABLE results (id SERIAL, project_id INTEGER references project(id), init_error TEXT, setup_error TEXT, setup_stdout TEXT, setup_stderr TEXT, build_error TEXT, build_stdout TEXT, build_stderr TEXT, test_error TEXT, test_stdout TEXT, test_stderr TEXT, started TIMESTAMP WITH TIME ZONE, ended TIMESTAMP WITH TIME ZONE, error BOOL, platform TEXT, created TIMESTAMP WITH TIME ZONE, updated TIMESTAMP WITH TIME ZONE)', next);
 };
 
 exports.down = function(next){
